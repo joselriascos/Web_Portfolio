98fa6d6268bf506c6c4b5b14ec5eb0d6cc35958b
jose luis riascos cubillos <joselriascos@unicauca.edu.co>
2025-02-18 17:08:15 -0500

    Moved Title component to App and updated styles for Header, Title, and WorkSection components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Footer } from './Components/Footer/Footer'
 import { Header } from './Components/Header/Header'
+import { Title } from './Components/Title/Title'
 import { WorkSection } from './Components/WorkSection/WorkSection'
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Title />
       <WorkSection />
       <Footer />
     </div>
--- src/Components/WorkSection/WorkSection.jsx
+++ src/Components/WorkSection/WorkSection.jsx
@@ -1,15 +1,9 @@
 import './WorkSection.css'
-import { Title } from '../Title/Title'
 import { ObservedAnimatedComponent } from '../ObservedAnimatedComponent'
 
 export function WorkSection() {
   return (
     <section className="work-section">
-      <Title />
-      <ObservedAnimatedComponent classIfVisible="put-closer" threshold={0}>
-        <div className="example" />
-      </ObservedAnimatedComponent>
-
       <ObservedAnimatedComponent classIfVisible="put-closer" threshold={0}>
         <div className="example" />
       </ObservedAnimatedComponent>
